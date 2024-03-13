18a5f657ff898574402bc89ce047545e77227516
Federico Kronhaus <federicokronhaus@gmail.com>
2024-03-13 11:28:50 -0300

    Log Add_Starred Service

--- app/Http/Controllers/StarredController.php
+++ app/Http/Controllers/StarredController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Starred_gifs;
+use App\Models\Log;
 
 class StarredController extends Controller
 {
@@ -14,7 +15,7 @@ class StarredController extends Controller
             "alias" => $request["alias"],
             "user_id" => $request["user_id"],
         ];
-
+        Log::create( [ 'service' => $request->path(), 'user' => $request["user_id"], 'request_body' => json_encode($request->all()), 'response_code' => 200, 'response_body' => '', 'source_ip' => $request->ip(), ] );
         $starred = Starred_gifs::create($params);
     }
 
